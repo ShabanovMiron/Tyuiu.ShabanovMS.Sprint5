caab1839d3762dcb6b7c060f00fe2ac538fd5d69
Tyuiu.ShabanovMS@outlook.com <shabanov2014@bk.ru>
2023-11-17 23:22:02 +0500

    Add project files.

---
+++ Tyuiu.ShabanovMS.Sprint5.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabanovMS.Sprint5.Task0.V21.Lib;
+using System.IO;
+namespace Tyuiu.ShabanovMS.Sprint5.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSave()
+        {
+            string path = @"C:\Users\Мирон\source\repos\Tyuiu.ShabanovMS.Sprint5\Tyuiu.ShabanovMS.Sprint5.Task0.V21\bin\Debug\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(fileExists, wait);
+        }
+    }
+}
---
+++ Tyuiu.ShabanovMS.Sprint5.Task0.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShabanovMS.Sprint5.Task0.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShabanovMS.Sprint5.Task0.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e285b053-64f1-4494-b6ab-c737f8ef9471")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
